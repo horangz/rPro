d356f325b50e57aed5f79d508ea97d678aab52bf
Dzeman <dzeman@rarus.ru>
2013-08-15 23:44:06 +0300

    Improve preferences

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+    
+    #returns the full title on a per-page basis.
+    def full_title(page_title)
+       base_title = "rPro"
+       if page_title.empty?
+           base_title
+       else
+           "#{base_title} | #{page_title}"
+       end
+    end    
+    
+    def least_common_multiple(number1, number2)
+        if number2 > number1
+            number1 = number1 ^ number2
+            number2 = number1 ^ number2
+        end
+        rest = number1 % number2
+        if rest == 0
+            return number2
+        end
+        least_common_multiple(number2, rest)
+    end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,27 +1,44 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+    
+  let(:base_title) { 'rPro' }
+    
   describe "Home page" do
-    it "shoul have the title 'rPro | Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-                                text: 'rPro | Home')
+                                text: "#{base_title}")
+    end
+    
+    it "should have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', 
+                                text: "| Home")
     end
   end
   
   describe "Help page" do
-    it "shoul have the title'rPro | Help'" do
+    it "should have the title'rPro | Help'" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-                                text: 'rPro | Help')
+                                text: "#{base_title} | Help")
     end
   end
   
   describe "About page" do
-    it "shoul have the title 'rPro | About'" do
+    it "should have the title 'rPro | About'" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-                                text: 'rPro | About')
+                                text: "#{base_title} | About")
+    end
+  end
+  
+  describe "Contact page" do
+    it "should have the title 'rPro | Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+                                 text: "#{base_title} | Contact")
     end
   end
 end
