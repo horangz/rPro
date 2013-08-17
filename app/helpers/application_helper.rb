e24cd9d8bcfea34cf4143922f045c41a0593bc47
Dzeman <dzeman@rarus.ru>
2013-08-17 17:35:34 +0300

    finish layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
     
     #returns the full title on a per-page basis.
     def full_title(page_title)
-       base_title = "rPro"
+       base_title = 'rPro'
        if page_title.empty?
            base_title
        else
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,43 +2,29 @@ require 'spec_helper'
 
 describe "StaticPages" do
     
-  let(:base_title) { 'rPro' }
+  subject { page }
     
   describe "Home page" do
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', 
-                                text: "#{base_title}")
-    end
-    
-    it "should have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', 
-                                text: "| Home")
-    end
+    before { visit root_path }
+      
+    it { page.should have_selector 'title', text: full_title('') }
   end
   
   describe "Help page" do
-    it "should have the title'rPro | Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', 
-                                text: "#{base_title} | Help")
-    end
+    before { visit help_path }
+      
+    it { page.should have_selector 'title', text: full_title('Help') }
   end
   
   describe "About page" do
-    it "should have the title 'rPro | About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', 
-                                text: "#{base_title} | About")
-    end
+    before { visit about_path }
+      
+    it { page.should have_selector 'title', text: full_title('About') }
   end
   
   describe "Contact page" do
-    it "should have the title 'rPro | Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', 
-                                 text: "#{base_title} | Contact")
-    end
+    before { visit contact_path }
+  
+    it { page.should have_selector 'title', text: full_title('Contact') }
   end
 end
